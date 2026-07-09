dc084be0bb39d95ec4dbc054b830b7fe68cd53f5
Pranjal Panwar <138666385+pranjalpanwar81@users.noreply.github.com>
2026-07-09 14:53:17 +0530

    Add cascade delete example using Mongoose middleware

---
+++ one-to-many/handlingdeletion.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Connect to MongoDB
+main()
+  .then(async () => {
+    console.log("✅ Connected to MongoDB");
+
+    await addCustomer();
+
+    mongoose.connection.close();
+  })
+  .catch(console.error);
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
+}
+
+// ====================
+// Order Schema
+// ====================
+
+const orderSchema = new Schema({
+  item: String,
+  price: Number,
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+// ====================
+// Customer Schema
+// ====================
+
+const customerSchema = new Schema({
+  name: String,
+  orders: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Order",
+    },
+  ],
+});
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+//Functions 
+const findCustomer = async () => {
+    let result = await Customer.find({}).populate("orders");
+    console.log(result[0]);
+}; 
+
+const addCustomer = async () => {
+    let newcust = new Customer({
+        name: "Pranjal"
+    });
+
+    let newOrder =  new Order({
+        item: "Laptop",
+        price: 1000
+    });
+
+    newcust.orders.push(newOrder);
+    await newOrder.save();
+    await newcust.save();
+
+    console.log("Customer and Order added successfully!");
+};
+
+const deleteCustomer = async () => {
+    let data = await Customer.findByIdAndDelete("6a4f48f918c14698a1d30cf5");
+    console.log(data);
+}
+
+deleteCustomer();
